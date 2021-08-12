8127fc4f8db475aba0f44feb34d425af69fbb2bf
Kevlyn Kadamala <kevlyn@gmail.com>
2021-08-12 17:29:45 +0530

    Working with timm

--- dataloader.py
+++ dataloader.py
@@ -1,5 +1,23 @@
+import torch
+
 import pandas as pd
+import numpy as np
 from PIL import Image
 
-df = pd.read_csv("dataset/captions.txt")
-print(df.head())
+
+class ImageDataset(torch.utils.data.Dataset):
+    def __init__(self, df, path, transform=None):
+        self.df = df
+        self.path = path
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        img = np.asarray(Image.open(self.path + self.df.image[idx]))
+
+        if self.transform:
+            img = self.transform(img)
+
+        return img
