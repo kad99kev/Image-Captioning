3f38906a08fd12659603a8e8486e68f45095b2d6
Kevlyn Kadamala <kevlyn@gmail.com>
2021-08-15 13:24:32 +0530

    Working on RNNDecoder

--- utils/models.py
+++ utils/models.py
@@ -1,7 +1,6 @@
-from audioop import bias
-from turtle import forward
 import torch
 import torch.nn as nn
+from typing import Tuple
 
 
 class CNNEncoder(nn.Module):
@@ -25,43 +24,64 @@ class CNNEncoder(nn.Module):
 
 
 class RNNDecoder(nn.Module):
-    def __init__(self, hidden_size, vocab_size, embedding_dim) -> None:
+    def __init__(
+        self, encoder_dim, decoder_dim, attention_dim, embedding_dim, vocab_size
+    ) -> None:
         super().__init__()
-        self.hidden_size = hidden_size
-
         self.embedding = nn.Embedding(vocab_size, embedding_dim)
-        self.gru = nn.GRUCell(embedding_dim, hidden_size)
-        self.fc_1 = nn.Linear(hidden_size, hidden_size)
-        self.fc_2 = nn.Linear(hidden_size, vocab_size)
+        self.gru = nn.GRUCell(encoder_dim + embedding_dim, decoder_dim)
+        self.fc_1 = nn.Linear(decoder_dim, decoder_dim)
+        self.fc_2 = nn.Linear(decoder_dim, vocab_size)
 
-        self.attn = Attention(hidden_size)
+        self.attn = Attention(encoder_dim, decoder_dim, attention_dim)
 
         self.softmax = nn.Softmax(dim=1)
 
-    def forward(self, x, features, hidden) -> torch.Tensor:
-        # Get batch size
-        batch_size = features.size(0)
+    def forward(self, decoder_input, features, hidden) -> torch.Tensor:
 
         # Get outputs from attention model
+        # context_vector: (1, embedding_dim)
+        # attn_weights: (1, 64, 1)
         context_vector, attention_weights = self.attn(features, hidden)
 
-        x = self.embedding(x)
+        # x: (1, 1, embedding_dim)
+        x = self.embedding(decoder_input)
 
+        # x: (1, 1, encoder_dim + embedding_dim)
+        x = torch.cat([context_vector.unsqueeze(1), x], dim=-1)
+        print(x.shape)
 
-# Reference: https://github.com/sgrvinod/a-PyTorch-Tutorial-to-Image-Captioning/blob/master/models.py
+        output, state = self.gru(x)
+        return output, state
 
 
 class Attention(nn.Module):
-    def __init__(self, hidden_size, encoder_size=None, decoder_size=None):
+    def __init__(self, encoder_dim, decoder_dim, attention_dim):
         super().__init__()
 
-        encoder_size = 2 * hidden_size if encoder_size is None else encoder_size
-        decoder_size = 2 * hidden_size if decoder_size is None else decoder_size
+        self.encoder_attn = nn.Linear(encoder_dim, attention_dim, bias=False)
+        self.decoder_attn = nn.Linear(decoder_dim, attention_dim, bias=False)
+        self.value = nn.Linear(attention_dim, 1, bias=False)
 
-        self.decoder_attn = nn.Linear(decoder_size, hidden_size, bias=False)
-        self.encoder_attn = nn.Linear(encoder_size, hidden_size, bias=False)
-        self.value = nn.Linear(hidden_size, 1, bias=False)
+        self.softmax = nn.Softmax(dim=1)
 
-    def forward(self, encoder_out, decoder_hidden):
-        enc_out = self.encoder_attn(encoder_out)
-        dec_out = self.decoder_attn(decoder_hidden)
+    def forward(self, encoder_out, decoder_hidden) -> Tuple[torch.Tensor, torch.Tensor]:
+        # hidden_: (batch_size, 1, encoder_dim)
+        hidden_ = decoder_hidden.unsqueeze(1)
+
+        # attn_hidden: (batch_size, 64, attention_dim)
+        attn_hidden = torch.tanh(
+            self.encoder_attn(encoder_out) + self.decoder_attn(hidden_)
+        )
+
+        # score: (1, 64, 1)
+        score = self.value(attn_hidden)
+
+        # attn_weights: (1, 64, 1)
+        attn_weights = self.softmax(score)
+
+        # context_vector after sum: (1, encoder_dim)
+        context_vector = attn_weights * encoder_out
+        context_vector = torch.sum(context_vector, 1)
+
+        return context_vector, attn_weights
